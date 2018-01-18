6fa24d37a8b6baae61de0776e916a8725e336efc
priyabrat.dev <priyabrat.dev@gmail.com>
2018-01-18 14:31:29 +0530

    This is a small change

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main {
 
 	public static void main(String []args){
 		System.out.println("Name is "+Utils.getFirstName()+" "+Utils.getLastName());
+		// This is a comment, commen1
+		System.out.println("Printing completed-----");
 	}
 }
---
+++ Priyabrat.java
@@ -0,0 +1,8 @@
+class Priyabrat {
+
+	public static void main(String []args){
+		System.out.println("I am Priyabrat Padhy");
+		// This is a commeny 2
+	}
+
+}
